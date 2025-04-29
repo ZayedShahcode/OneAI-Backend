12762c7db5a9dee8c957d565288f9c9635f51890
ZayedShahcode <reachzayed@proton.me>
2025-04-29 18:10:20 +0530

    port binding

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 app = create_app()
+port = os.getenv('PORT', 5000)
 
 if __name__== '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+    app.run(debug=False, host='0.0.0.0', port=int(port)) 
